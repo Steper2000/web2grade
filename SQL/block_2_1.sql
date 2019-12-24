38e5d3e6f039086af6b05b238031af5d957897b4
Steper2000 <okunev-85@bk.ru>
2019-12-24 15:19:41 +0300

    again bugfix

--- SQL/block_2_1.sql
+++ SQL/block_2_1.sql
@@ -2,6 +2,6 @@
 чем на 5000 рублей."
 
 SELECT plogin, sum(price) as sprice FROM `services` 
-WHERE ptime BETWEEN '2013-01-01' and '2013-12-31' 
+WHERE ptime BETWEEN '2013-01-01' and '2013-12-31 23:59:59' 
 GROUP BY plogin 
 HAVING SUM(price) > 5000
--- SQL/block_2_2.sql
+++ SQL/block_2_2.sql
@@ -3,11 +3,12 @@
 
 select pservice 
 from services 
-WHERE ptime BETWEEN "2013-01-01" and "2013-12-31" 
+WHERE ptime BETWEEN "2013-01-01" and "2013-12-31 23:59:59" 
 GROUP by pservice 
-HAVING Count(pservice)=(SELECT COUNT(pservice) 
-                        FROM `services`
-                        WHERE ptime BETWEEN "2013-01-01" and "2013-12-31" 
+HAVING Count(pservice)=(SELECT  COUNT(pservice) as f 
+                        FROM `services` 
+                        WHERE ptime 
+                        BETWEEN "2013-01-01" and "2013-12-31 23:59:59" 
                         GROUP by pservice 
-                        ORDER by pservice 
+                        ORDER by f desc
                         LIMIT 1)
